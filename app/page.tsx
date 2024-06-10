354d23f8581c9ef0c6a43289d0759fcefe75625c
Donovan <donovan.courtney@gmail.com>
2024-06-09 20:48:17 -0500

    scroll line simplified with callback when pathLength is complete

--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,17 @@ import { Card, Flex, Text, Box, Container } from "@radix-ui/themes";
 
 export default function Home() {
   return (
-    <Flex direction={"column"} justify={"center"} align={"center"}>
+    <Flex direction={"column"} align={"center"}>
       <Box minWidth={{ initial: "100%", md: "75%", lg: "50%" }}>
-        <FullNameSVG />
-        <Box position={"relative"} height={"100vh"} className="bg-gray-800">
-          <Box position={"absolute"} left={"50%"} top={"0"}>
-            <ScrollLineSVG />
-          </Box>
+        <Box>
+          <FullNameSVG />
         </Box>
+        <Box position={"relative"} width={"100%"} mt="-1">
+          <ScrollLineSVG />
+        </Box>
+        
       </Box>
-      <Box>
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
-          nihil voluptas repellendus itaque eaque facilis minima facere
-          quibusdam minus consectetur perspiciatis aliquid iure quasi placeat
-          nulla ex dolore, ipsam expedita.
-        </Text>
-      </Box>
+  
     </Flex>
   );
 }
